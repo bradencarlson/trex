7264daeeefd0eddb0cb1f8bd98f2b0c222173d6e
Braden Carlson <bradenjcarlson@live.com>
2025-09-07 20:33:22 -0700

    Option<Table> return from config
    changed the return type from the config methods to be Options instead of
    values. This will mean that the caller has to perform some error
    handling, but this makes it easier to implement the functions in this
    module.

--- src/config.rs
+++ src/config.rs
@@ -6,25 +6,24 @@ pub const DEFAULT_CONFIG: &str = "trex.toml";
 const MINIMAL_CONFIG: &str = "[files]
 1 = test.tex";
 
-pub fn read(path: &str) -> Table  {
+pub fn read(path: &str) -> Option<Table>  {
     match fs::read_to_string(&path) {
         Ok(content) => parse(&content),
         Err(_) => create_config_and_parse(),
     }
 }
 
-fn parse(content: &String) -> Table {
-    let result = toml::from_str(content);
-    match result {
+fn parse(content: &String) -> Option<Table> {
+    match toml::from_str(content) {
         Ok(table) => table,
-        Err(_) => {
-            std::process::exit; 
-            toml::from_str(MINIMAL_CONFIG).expect("")
-        },
+        Err(_) => None,
     }
 }
 
-fn create_config_and_parse() -> Table {
-    toml::from_str(MINIMAL_CONFIG).expect("")
+fn create_config_and_parse() -> Option<Table> {
+    match toml::from_str(MINIMAL_CONFIG) {
+        Ok(table) => table,
+        Err(_) => None
+    }
 }
 
