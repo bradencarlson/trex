9517fe89e1f2e33ac8b89f511f2a61e455421615
Braden Carlson <bradenjcarlson@live.com>
2025-09-18 22:22:44 -0700

    Lib File
    Separate logic into lib to implement testing in the future.

---
+++ src/lib.rs
@@ -0,0 +1,34 @@
+use std::collections::HashMap;
+
+mod parse_args;
+mod config;
+mod outline;
+mod compile;
+
+pub fn run(args: &HashMap<String,String>) {
+
+    let filename = match args.get(parse_args::FILENAME_ARG) {
+        Some(string) => string,
+        None => config::DEFAULT_CONFIG,
+    };
+
+    let outline = match config::read(filename) {
+        Some(outline) => outline, 
+        None => return,
+    };
+
+    println!("The outline I read was:\n\n{}\n", outline);
+
+    match args.get(parse_args::COMPILE_ARG) {
+        Some(_string) => {
+            compile::compile(outline, &args);
+        },
+        None => {
+            /* The default behavior is to NOT compile the document, this is to prevent 
+             * firing up pdflatex every time I want to test something. In a
+             * future version compiling will be the default. */
+            println!("Pass the -c option in order to compile.");     
+        },
+    }
+
+}
--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,13 @@
 use std::env;
 
-pub mod parse_args;
-mod config;
-mod compile;
-pub mod outline;
+mod parse_args;
+
+use trex;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
     let args_map = parse_args::parse(args);
 
-    let filename = match args_map.get(parse_args::FILENAME_ARG) {
-        Some(string) => string,
-        None => config::DEFAULT_CONFIG,
-    };
-
-    let outline = match config::read(filename) {
-        Some(outline) => outline, 
-        None => return,
-    };
-
-    println!("The outline I read was:\n\n{}\n", outline);
-
-    match args_map.get(parse_args::COMPILE_ARG) {
-        Some(_string) => {
-            compile::compile(outline, &args_map);
-        },
-        None => {
-            /* The default behavior is to NOT compile the document, this is to prevent 
-             * firing up pdflatex every time I want to test something. In a
-             * future version compiling will be the default. */
-            println!("Pass the -c option in order to compile.");     
-        },
-    }
+    trex::run(&args_map)
 }
