7050354341b2838513efa76dfb5177f540487a8d
Braden Carlson <bradenjcarlson@live.com>
2025-09-29 21:43:33 -0700

    More simple tests
    Added two more simple tests, one for the range arg, and one for the
    engine arg.

--- tests/args_tests.rs
+++ tests/args_tests.rs
@@ -8,3 +8,16 @@ fn filename_arg() {
     assert!(parse_args::parse_argument(&k,&v));
 }
 
+#[test]
+fn range_arg() {
+    let r = String::from("-r");
+    let v = String::from("1-3");
+    assert!(parse_args::parse_argument(&r, &v));
+}
+
+#[test]
+fn engine_arg() {
+    let k = String::from("-e");
+    let v = String::from("pdflatex");
+    assert!(parse_args::parse_argument(&k, &v));
+}
