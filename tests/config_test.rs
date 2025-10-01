d8015e50073aac2b5155f52d6fce6cbc61b741be
Braden Carlson <bradenjcarlson@live.com>
2025-09-30 21:57:03 -0700

    Config Test
    Added a test to make sure that a sample config file is read correctly.
    This simply tests to make sure the three files are detected, the two
    sections are found, and that the preamble is detected.

--- tests/config_test.rs
+++ tests/config_test.rs
@@ -25,3 +25,29 @@ fn default_config_test() {
     assert_eq!(none, "failed");
 
 }
+
+fn config_test() {
+    let outline = config::read("sample_config.txt").unwrap();
+    let preamble = match outline.get_preamble() {
+        Some(s) => s, 
+        None => "failed",
+    };
+    let file_one = outline.get_lecture(0)
+        .expect("Sample config should have a file.");
+
+    let file_two = outline.get_lecture(1)
+        .expect("Sample config should have a file here.");
+
+    let file_three = outline.get_lecture(2)
+        .expect("Sample config should have a file here.");
+
+    let num_sections = outline.section_positions.len();
+    let num_sec_names = outline.section_names.len();
+
+    assert_eq!(preamble, "testing.tex");
+    assert_eq!(file_one, "first-lecture.tex");
+    assert_eq!(file_two, "second-lecture.tex");
+    assert_eq!(file_three, "third-lecture.tex");
+    assert_eq!(num_sections, 2);
+    assert_eq!(num_sec_names, 2);
+}
