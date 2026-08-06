2eefcad173b03fb6ad945babf052f28bec672a10
Braden Carlson <bradenjcarlson@live.com>
2026-08-06 15:23:37 -0700

    Remove `.out` files
    These also need to be cleaned up.

--- src/compile/tex.rs
+++ src/compile/tex.rs
@@ -26,7 +26,7 @@ pub fn clean() {
     /* Cleans up auxiliary files created when compiling a tex document. */
 
     let mut files = Vec::<String>::new();
-    let file_types = vec![".aux", ".log", ".toc", ".bbl", ".blg"];
+    let file_types = vec![".out", ".aux", ".log", ".toc", ".bbl", ".blg"];
     if let Ok(dir) = fs::read_dir(".") {
         for entry in dir {
             if let Ok(e) = entry {
